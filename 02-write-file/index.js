325add064fa9b23209e93bdbec972e15b2643f82
Mary Zin <mary22091991mary@gmail.com>
2024-01-21 18:15:47 +0300

    feat: change solution of 02-write-file to a solution with promises

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,12 +7,9 @@ const read = readline.createInterface({
   output: process.stdout,
 });
 
-fs.writeFile(path.resolve(__dirname, '02-write-file.txt'), '', (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-});
+fs.promises
+  .writeFile(path.resolve(__dirname, '02-write-file.txt'), '')
+  .catch((err) => console.log(err));
 
 console.log('Привет! Введите текст, который хотите записать:');
 
@@ -21,14 +18,7 @@ read.on('line', (answer) => {
     console.log('До новых встреч!');
     process.exit();
   }
-  fs.appendFile(
-    path.resolve(__dirname, '02-write-file.txt'),
-    `${answer}\n`,
-    (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-    },
-  );
+  fs.promises
+    .appendFile(path.resolve(__dirname, '02-write-file.txt'), `${answer}\n`)
+    .catch((err) => console.log(err));
 });
