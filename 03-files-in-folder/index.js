47b1154ba7f6eca227bc9934d2ca21fe49cacd0a
Mary Zin <mary22091991mary@gmail.com>
2024-01-21 18:52:46 +0300

    feat: change solution of 03-files-in-folder to a solution with promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,27 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(
-  path.resolve(__dirname, 'secret-folder'),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) {
-      console.log(err);
-    } else {
-      for (let i = 0; i < files.length; i++) {
-        if (!files[i].isDirectory()) {
-          fs.stat(
-            path.resolve(__dirname, 'secret-folder', files[i].name),
-            (err, stat) => {
-              console.log(
-                `${path.parse(files[i].name).name} - ${path
-                  .parse(files[i].name)
-                  .ext.slice(1)} - ${stat.size} bytes`,
-              );
-            },
-          );
-        }
+fs.promises
+  .readdir(path.resolve(__dirname, 'secret-folder'), { withFileTypes: true })
+  .then((files) => {
+    for (let i = 0; i < files.length; i++) {
+      if (!files[i].isDirectory()) {
+        fs.promises
+          .stat(path.resolve(__dirname, 'secret-folder', files[i].name))
+          .then((stat) => {
+            console.log(
+              `${path.parse(files[i].name).name} - ${path
+                .parse(files[i].name)
+                .ext.slice(1)} - ${stat.size} bytes`,
+            );
+          });
       }
     }
-  },
-);
+  })
+  .catch((err) => console.log(err));
