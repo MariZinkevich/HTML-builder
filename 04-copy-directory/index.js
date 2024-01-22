78ded00d068aabd86daafc654b3018845f06e731
Mary Zin <mary22091991mary@gmail.com>
2024-01-22 22:17:57 +0300

    feat: change solution of 04-copy-directory  to a solution with promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.promises
-  .readdir(path.resolve(__dirname))
-  .then((files) => {
-    if(files.includes('files-copy')){
-      fs.promises.rm(path.resolve(__dirname, 'files-copy'), {recursive: true});
-    }
-    })
+fs.promises.readdir(path.resolve(__dirname))
+.then((files) => {
+  if(files.includes('files-copy')){
+    fs.promises.rm(path.resolve(__dirname, 'files-copy'), {recursive: true})
+    .then(startCopy)
+  } else {
+    startCopy();
+  }
+})
+
+    
+
+
+function startCopy(){
+  fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true })
   .then(()=>{
-    fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
-        if (err) {
-          console.log(err);
-        }
-    });
-    fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
-      if (err) {
-        console.log(err);
-      } else {
-        for (let i = 0; i < files.length; i++) {
-          fs.copyFile(
-            path.resolve(__dirname, 'files', files[i]),
-            path.resolve(__dirname, 'files-copy', files[i]),
-            (err) => {
-              if (err) {
-                console.log(err);
-              }
-            },
-          );
-        }
-        console.log('Копирование завершено');
+    fs.promises.readdir(path.resolve(__dirname, 'files'))
+    .then((files) => {
+      for (let i = 0; i < files.length; i++) {
+        fs.copyFile(
+          path.resolve(__dirname, 'files', files[i]),
+          path.resolve(__dirname, 'files-copy', files[i]),
+          (err) => {
+            if (err) {
+              console.log(err);
+            }
+          },
+        );
       }
-      });
     });
+  })
+}
 
 
