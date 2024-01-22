8c3e82b48aa6e2adb91a56532505cdfbce0fb541
Mary Zin <mary22091991mary@gmail.com>
2024-01-22 20:02:31 +0300

    feat: change solution of  05-merge-styles to a solution with promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,31 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFile(path.resolve(__dirname, 'project-dist', 'bundle.css'), '', (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-});
+fs.promises.writeFile(path.resolve(__dirname, 'project-dist', 'bundle.css'), '')
+.catch(err => console.log(err))
 
-fs.readdir(path.resolve(__dirname, 'styles'), (err, files)=>{
-    if(err) {
-        console.log(err)
-    } else {
-        for(let i = 0; i < files.length; i++){
-            if(path.parse(files[i]).ext === '.css') {
-                fs.readFile(path.resolve(__dirname, 'styles', files[i]), 'utf-8', (err, data)=>{
-                    if(err) {
-                        console.log(err)
-                    } else {
-                        fs.appendFile(path.resolve(__dirname, 'project-dist', 'bundle.css'), data, (err) => {
-                            if(err) {
-                                console.log(err)
-                            }
-                        })
-                    }
-                })
-            }
-        }
+fs.promises.readdir(path.resolve(__dirname, 'styles'))
+ .then((files) => {
+  for (let i = 0; i < files.length; i++) {
+    if (path.parse(files[i]).ext === '.css') {
+      fs.promises
+      .readFile(path.resolve(__dirname, 'styles', files[i]),'utf-8')
+      .then(data => {       
+        fs.promises.appendFile(path.resolve(__dirname, 'project-dist', 'bundle.css'),data)})
     }
+  }
 })
