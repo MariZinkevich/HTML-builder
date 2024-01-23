f4d6372aa3fb6f576ea90c6d4085ea6b746afefb
Mary Zin <mary22091991mary@gmail.com>
2024-01-23 07:42:18 +0300

    fix: delete comment for reviewer

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,15 +1,1 @@
-//ребяяяяят, если есть возможность дайте пожалйста времени до 23.01 до 19.00, я не успела(((
-
-const fs = require('fs');
-const path = require('path');
-
-
-
-fs.promises.readFile(path.resolve(__dirname, 'template.html'), 'utf-8')
-.then((template)=>{
-    fs.promises.readFile(path.resolve(__dirname, 'components', 'header.html'), 'utf-8')
-    .then((header) =>{
-        let concatHeader = template.replace('{{header}}', header);
-    })
-})
 
